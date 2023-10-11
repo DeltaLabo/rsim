a5cd4250cfb91e17c0def3dac4c84b7511ec74a4
anthonyarguedas <anthony_arguedas@outlook.com>
2023-10-11 00:04:43 -0600

    Add total time calculator

--- delay_compensation.py
+++ delay_compensation.py
@@ -4,14 +4,20 @@ period = 1.0
 delay = 0.4
 #period = 0.125
 #delay = 0.052
-tolerance = 0.1
+tolerance = 0.2
+N = 0
+number_of_measurements = 500
 
 for n in range(1, 20):
     if abs(ceil(delay * n) - delay * n) <= abs(floor(delay * n) - delay * n):
         if abs(period * ceil(delay * n / period) - delay * n) <= tolerance:
-            print(f"n = {n}, period * k = {period * ceil(delay * n / period)}, delay * n = {delay * n}, error = {abs(period * ceil(delay * n / period) - delay * n)}")
+            print(f"n = {n}, period * m = {period * ceil(delay * n / period)}, delay * n = {delay * n}, error = {abs(period * ceil(delay * n / period) - delay * n)}")
+            N = n
             break
     elif abs(ceil(delay * n) - delay * n) > abs(floor(delay * n) - delay * n):
         if abs(period * floor(delay * n / period) - delay * n) <= tolerance:
-            print(f"n = {n}, period * k = {period * floor(delay * n / period)}, delay * n = {delay * n}, error = {abs(period * floor(delay * n / period) - delay * n)}")
+            print(f"n = {n}, period * m = {period * floor(delay * n / period)}, delay * n = {delay * n}, error = {abs(period * floor(delay * n / period) - delay * n)}")
+            N = n
             break
+
+print(f"time multiplier: {(1+N) - N/number_of_measurements}, total time = {period*number_of_measurements*((1+N)-N/number_of_measurements)}")
