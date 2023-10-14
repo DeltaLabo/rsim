236276ed1e0bea2ea1306dd1cf157389a5b95c30
anthonyarguedas <anthony_arguedas@outlook.com>
2023-10-13 23:25:19 -0600

    Create usbtoi2s.ino

---
+++ usbtoi2s/usbtoi2s.ino
@@ -0,0 +1,13 @@
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioInputUSB            usb1;           //xy=119.42855834960938,189.4285659790039
+AudioOutputI2Sslave      i2sslave1;      //xy=283.42866134643555,189.42855834960938
+AudioConnection          patchCord1(usb1, 0, i2sslave1, 0);
+AudioConnection          patchCord2(usb1, 1, i2sslave1, 1);
+// GUItool: end automatically generated code
+
