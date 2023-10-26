250aa58c6c5a88bd41922bf418ecde76f8bf29f4
anthonyarguedas <anthony_arguedas@outlook.com>
2023-10-26 15:02:38 -0600

    Add calibration scripts

---
+++ calibration/test_waves.py
@@ -0,0 +1,12 @@
+linearity_test_waves = [
+    {
+        "duration": 10,
+        "frequency": 31.5,
+        "amplitude": 0.5
+    },
+    {
+        "duration": 10,
+        "frequency": 61.0,
+        "amplitude": 0.5
+    },
+]
