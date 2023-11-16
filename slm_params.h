ee48f4d08ea4ee841faf152e15b7b19aeb34904e
anthonyarguedas <anthony_arguedas@outlook.com>
2023-11-16 17:35:03 -0600

    Run 3M calibration

--- slm_params.h
+++ slm_params.h
@@ -4,10 +4,10 @@
 #ifndef PARAMS_H
 #define PARAMS_H
 
-#define LEQ_PERIOD        0.125       // second(s)
+#define LEQ_PERIOD        1.0       // second(s)
 #define WEIGHTING         A_weighting // 'A_weighting' 'C_weighting' or 'None' (Z_weighting)
 #define LEQ_UNITS         "LAeq"      // customize based on above weighting used
 #define DB_UNITS          "dBA"       // customize based on above weighting used
-#define MIC_OFFSET_DB     -2.3         // Offset (sine-wave RMS vs. dBFS). Modify this value for linear calibration. Default is 3.0103
+#define MIC_OFFSET_DB     2.9         // Offset (sine-wave RMS vs. dBFS). Modify this value for linear calibration. Default is 3.0103
 
 #endif
