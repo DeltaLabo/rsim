0051f2a9cc5cd928eee76d36f943d4233b5bbf4a
anthonyarguedas <anthony_arguedas@outlook.com>
2023-12-02 01:29:21 -0600

    Test code for calibration

---
+++ calibration/esp_calibration/slm_params.h
@@ -0,0 +1,16 @@
+// Parameter file for the dB meter
+// These are in a separate file to avoid updating the versioned main.ino file just to change them
+
+#ifndef PARAMS_H
+#define PARAMS_H
+
+#define GREEN_UPPER_LIMIT 67.0
+#define YELLOW_UPPER_LIMIT 77.0
+
+#define LEQ_PERIOD        0.125      // second(s)
+#define WEIGHTING         A_weighting // 'A_weighting' 'C_weighting' or 'None' (Z_weighting)
+#define LEQ_UNITS         "LAeq"      // customize based on above weighting used
+#define DB_UNITS          "dBA"       // customize based on above weighting used
+#define MIC_OFFSET_DB     0.0         // Offset (sine-wave RMS vs. dBFS). Modify this value for linear calibration. Default is 3.0103
+
+#endif
