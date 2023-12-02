81bf8a91415021015eac475f7b711615ab3e0d63
anthonyarguedas <anthony_arguedas@outlook.com>
2023-12-02 01:29:57 -0600

    Update params after calibration

--- slm_params.h
+++ slm_params.h
@@ -4,13 +4,15 @@
 #ifndef PARAMS_H
 #define PARAMS_H
 
-#define GREEN_UPPER_LIMIT 67.0
-#define YELLOW_UPPER_LIMIT 77.0
+#define GREEN_UPPER_LIMIT  67.0 // dBA, maximum noise level for which the indicator stays green
+#define YELLOW_UPPER_LIMIT 77.0 // dBA, maximum noise level for which the indicator stays yellow
+
+#define LOGGING_PERIOD 64.0 // second(s)
 
 #define LEQ_PERIOD        8.0      // second(s)
 #define WEIGHTING         A_weighting // 'A_weighting' 'C_weighting' or 'None' (Z_weighting)
 #define LEQ_UNITS         "LAeq"      // customize based on above weighting used
 #define DB_UNITS          "dBA"       // customize based on above weighting used
-#define MIC_OFFSET_DB     2.9         // Offset (sine-wave RMS vs. dBFS). Modify this value for linear calibration. Default is 3.0103
+#define MIC_OFFSET_DB     1.3         // Offset (sine-wave RMS vs. dBFS). Modify this value for linear calibration.
 
 #endif
