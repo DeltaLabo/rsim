319d644ff1de7aa9e5b9dc9b22637c2eece2319e
anthonyarguedas <anthony_arguedas@outlook.com>
2024-08-17 14:16:53 -0600

    Update pins for PCB

--- pins.h
+++ pins.h
@@ -7,8 +7,8 @@
 #define I2S_SCK 12
 #define I2S_SD  11
 
-#define RED_LED_PIN 21//15 on PCB
-#define GREEN_LED_PIN 35//16 on PCB
+#define RED_LED_PIN 15
+#define GREEN_LED_PIN 16
 #define BLUE_LED_PIN 17
 
 #define CHARGER_LED_PIN 4
