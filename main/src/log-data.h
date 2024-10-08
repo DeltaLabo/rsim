805bd3cee04b20c3db9b4d4f696a0a173ad16d74
anthonyarguedas <anthony_arguedas@outlook.com>
2024-10-08 17:55:26 -0600

    Create log-data.h

---
+++ main/src/log-data.h
@@ -0,0 +1,13 @@
+// log_data.h
+#ifndef LOG_DATA_H
+#define LOG_DATA_H
+
+#include <Arduino.h>
+
+// Container class for logged values, with their Adafruit IO key
+struct LogData {
+    String value;
+    String feedKey;
+};
+
+#endif // LOG_DATA_H
