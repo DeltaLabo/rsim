52afa994c0c001b974065db751a9b95b9f2780d1
anthonyarguedas <anthony_arguedas@outlook.com>
2024-10-08 17:56:25 -0600

    Move battery checker functionality to separate file

---
+++ main/src/battery-checker.h
@@ -0,0 +1,22 @@
+#ifndef BATTERY_CHECKER_H
+#define BATTERY_CHECKER_H
+
+#include <Adafruit_INA219.h>
+#include <Wire.h>
+
+#include "pins.h"
+
+// Power constants
+#define MIN_CHARGING_CURRENT 200 // mA
+#define MIN_CHARGED_VOLTAGE 12.8 // V
+
+// Power states
+#define LOW_BATTERY 0
+#define ENOUGH_BATTERY 1
+#define CHARGING 2
+
+#define BATTERY_CHECK_PERIOD 60000 // ms, 1 minute
+
+void battery_checker_task(void* parameter);
+
+#endif // BATTERY_CHECKER_H
