e952f9bdb5cfb4d00b5d9a8eec48c848bc192550
anthonyarguedas <anthony_arguedas@outlook.com>
2024-10-08 17:55:13 -0600

    Remove SLM functionality from main

--- main/main.ino
+++ main/main.ino
@@ -1,45 +1,20 @@
 #include <Arduino.h>
 
-#include "driver/i2s.h"
-#include "driver/ledc.h"
-#include <Wire.h>
-#include <Adafruit_INA219.h>
-#include <String.h>
-
-#include "src/sos-iir-filter-xtensa.h"
-#include "src/slm-params.h"
-#include "src/pins.h"
-#include "src/mic-params.h"
+#include "src/slm.h"
 #include "src/color-control.h"
 #include "src/adafruit-io.h"
+#include "src/battery-checker.h"
+#include "src/sos-iir-filter-xtensa.h"
 
-// I2S peripheral to use (0 or 1)
-#define I2S_PORT          I2S_NUM_0
 
-// Previous local color indication
-// Used to avoid setting the LEDs to the same color
-// they already were
-// Initialized to a null value
-int prevColor = -1;
+// Comment to disable functionality
+//#define USE_BATTERY
+#define USE_LOGGING
 
-// Current color being displayed on the LEDs
-// Initialized to a null value
-int currentColor = -1;
-
-// Last measurements collected, converted to color
-int colorArray[COLOR_WINDOW_SIZE];
-// Flag to reset the values stored in the array
-bool initColorArray = true;
-
-// Battery voltage and current meter
-Adafruit_INA219 ina219;
-
-short batteryState = ENOUGH_BATTERY;
 
 QueueHandle_t samples_queue;
+QueueHandle_t logging_queue;
 
-// Static buffer for block of samples
-float samples[SAMPLES_SHORT] __attribute__((aligned(4)));
 
 // DC-Blocker filter - removes DC component from I2S data
 // See: https://www.dsprelated.com/freebooks/filters/DC_Blocker.html
@@ -87,271 +62,30 @@ SOS_IIR_Filter A_weighting = {
   }
 };
 
-// I2S Microphone sampling setup 
-void mic_i2s_init() {
-  // Setup I2S to sample mono channel for SAMPLE_RATE * SAMPLE_BITS
-  const i2s_config_t i2s_config = {
-    mode: i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
-    sample_rate: SAMPLE_RATE,
-    bits_per_sample: i2s_bits_per_sample_t(SAMPLE_BITS),
-    channel_format: I2S_CHANNEL_FMT_ONLY_LEFT,
-    communication_format: i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
-    intr_alloc_flags: ESP_INTR_FLAG_LEVEL1,
-    dma_buf_count: DMA_BANKS,
-    dma_buf_len: DMA_BANK_SIZE,
-    use_apll: true,
-    tx_desc_auto_clear: false,
-    fixed_mclk: 0
-  };
-  // I2S pin mapping
-  const i2s_pin_config_t pin_config = {
-    bck_io_num:   I2S_SCK,  
-    ws_io_num:    I2S_WS,
-    data_out_num: -1, // not used
-    data_in_num:  I2S_SD   
-  };
-
-  i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
-
-  #if (MIC_TIMING_SHIFT > 0) 
-    // Undocumented (?!) manipulation of I2S peripheral registers
-    // to fix MSB timing issues with some I2S microphones
-    REG_SET_BIT(I2S_TIMING_REG(I2S_PORT), BIT(9));   
-    REG_SET_BIT(I2S_CONF_REG(I2S_PORT), I2S_RX_MSB_SHIFT);  
-  #endif
-  
-  i2s_set_pin(I2S_PORT, &pin_config);
-}
-
-// I2S Reader Task
-// Rationale for separate task reading I2S is that IIR filter
-// processing cam be scheduled to different core on the ESP32
-// while main task can do something else
-//
-// As this is intended to run as separate high-priority task, 
-// we only do the minimum required work with the I2S data
-// until it is 'compressed' into sum of squares 
 
 // FreeRTOS priority and stack sizes (in 32-bit words) 
 #define I2S_TASK_PRI   4
-#define I2S_TASK_STACK 2048
+#define I2S_TASK_STACK 4096
 #define LEQ_TASK_PRI   3
 #define LEQ_TASK_STACK 8096
 #define BAT_TASK_PRI 1
 #define BAT_TASK_STACK 2048
 #define WIFI_TASK_PRI 1
 #define WIFI_TASK_STACK 2048
-
-void mic_i2s_reader_task(void* parameter) {
-  mic_i2s_init();
-
-  // Discard first block, microphone may have startup time (i.e. INMP441 up to 83ms)
-  size_t bytes_read = 0;
-  i2s_read(I2S_PORT, &samples, SAMPLES_SHORT * sizeof(int32_t), &bytes_read, portMAX_DELAY);
-
-  while (true) {
-    // Block and wait for microphone values from I2S
-    //
-    // Data is moved from DMA buffers to our 'samples' buffer by the driver ISR
-    // and when there is requested ammount of data, task is unblocked
-    //
-    // Note: i2s_read does not care it is writing in float[] buffer, it will write
-    //       integer values to the given address, as received from the hardware peripheral. 
-    i2s_read(I2S_PORT, &samples, SAMPLES_SHORT * sizeof(SAMPLE_T), &bytes_read, portMAX_DELAY);
-    
-    // Convert (including shifting) integer microphone values to floats, 
-    // using the same buffer (assumed sample size is same as size of float), 
-    // to save a bit of memory
-    SAMPLE_T* int_samples = (SAMPLE_T*)&samples;
-    for(int i=0; i<SAMPLES_SHORT; i++) samples[i] = MIC_CONVERT(int_samples[i]);
-
-    // Apply DC blocker,
-    // writes filtered samples back to the same buffer.
-    float sum_sqr_weighted = DC_BLOCKER.filter(samples, samples, SAMPLES_SHORT);
-
-    // Apply equalization and calculate Z-weighted sum of squares
-    sum_sqr_weighted = INMP441.filter(samples, samples, SAMPLES_SHORT);
-
-    // Apply weighting and calculate weigthed sum of squares
-    sum_sqr_weighted = A_weighting.filter(samples, samples, SAMPLES_SHORT);
-
-    // Send the sums to FreeRTOS queue where main task will pick them up
-    // and further calcualte decibel values (division, logarithms, etc...)
-    xQueueSend(samples_queue, &sum_sqr_weighted, portMAX_DELAY);
-  }
-}
-
-void leq_calculator_task(void* parameter) {
-  // Queue object for microphone data
-  float sum_sqr_weighted;
-  // Counter for the amount of samples read in a single measurement period
-  uint32_t Leq_samples = 0;
-  // Counter for the amount of samples read in a single logging period
-  uint32_t Logging_samples = 0;
-  // Variable to store the sum of squares of a single measurement batch
-  double Leq_sum_sqr = 0;
-  // Variable to store the sum of squares of a single logging batch
-  double Logging_sum_sqr = 0;
-  // Final noise level value in dB (with the selected weighting applied)
-  double Leq_dB = 0;
-  // Final noise level value for one logging period in dB (with the selected weighting applied)
-  double Logging_leq = 0;
-  // Maximum noise level within one logging period
-  // Initialized to a value that's lower than any possible measurement
-  double Max_leq = MIC_NOISE_DB - 1.0;
-  // Minimum noise level within one logging period
-  // Initialized to a value that's higher than any possible measurement
-  double Min_leq = MIC_OVERLOAD_DB + 1.0;
-
-  // Read sum of samples, calculated by 'i2s_reader_task'
-  while (true) {
-    if (xQueueReceive(samples_queue, &sum_sqr_weighted, portMAX_DELAY)) {
-      // Accumulate Leq sum
-      Leq_sum_sqr += sum_sqr_weighted;
-      // Update the amount of samples read
-      Leq_samples += SAMPLES_SHORT;
-    }
-
-    // When we gather enough samples, calculate new Leq value 
-    if (Leq_samples >= SAMPLE_RATE * LEQ_PERIOD) {
-      // Calculate RMS of the equivalent sound level
-      double Leq_RMS = sqrt(Leq_sum_sqr / Leq_samples);
-      // Calculate sound level in decibels, with respect to the microphone reference
-      Leq_dB = MIC_OFFSET_DB + MIC_REF_DB + 20 * log10(Leq_RMS / MIC_REF_AMPL);
-
-      // In case of acoustic overload or below noise floor measurement, report limit Leq value
-      if (Leq_dB > MIC_OVERLOAD_DB) Leq_dB = MIC_OVERLOAD_DB;
-      else if (Leq_dB < MIC_NOISE_DB) Leq_dB = MIC_NOISE_DB;
-
-      // Update max and min values, if needed
-      if (Leq_dB < Min_leq) Min_leq = Leq_dB;
-      if (Leq_dB > Max_leq) Max_leq = Leq_dB;
-
-      currentColor = leqToColor(Leq_dB);
-      currentColor = updateColorArray(currentColor);
-
-      if (currentColor != prevColor) {
-        prevColor = currentColor;
-        setLEDColor(currentColor);
-      }
-
-      Serial.print("[INFO] [SLM]: Local reading (dBA): ");
-      Serial.print(Leq_dB);
-      Serial.print(", Color: ");
-      Serial.println(currentColor);
-
-      // ThingSpeak data calculation
-      // Accumulate Leq sum
-      Logging_sum_sqr += Leq_sum_sqr;
-      // Update the amount of samples read
-      Logging_samples += Leq_samples;
-
-      // When a new dB measurement is ready
-      if (Logging_samples >= SAMPLE_RATE * LOGGING_PERIOD) {
-        // Calculate RMS of the equivalent sound level for the entire logging period
-        double Logging_leq_RMS = sqrt(Logging_sum_sqr / Logging_samples);
-        // Calculate sound level in decibels, with respect to the microphone reference
-        Logging_leq = MIC_OFFSET_DB + MIC_REF_DB + 20 * log10(Logging_leq_RMS / MIC_REF_AMPL);
-
-        // In case of acoustic overload or below noise floor measurement, report limit Leq value
-        if (Logging_leq > MIC_OVERLOAD_DB) Logging_leq = MIC_OVERLOAD_DB;
-        else if (Logging_leq < MIC_NOISE_DB) Logging_leq = MIC_NOISE_DB;
-
-        #ifdef USE_LOGGING
-        logToAdafruitIO(String(Logging_leq), eq_feed_key);
-        logToAdafruitIO(String(Max_leq), max_feed_key);
-        logToAdafruitIO(String(Min_leq), min_feed_key);
-        #endif
-
-        // Reset the sum of squares and sample counter
-        Logging_sum_sqr = 0;
-        Logging_samples = 0;
-        // Reset max and min values
-        Max_leq = MIC_NOISE_DB - 1.0;
-        Min_leq = MIC_OVERLOAD_DB + 1.0;
-      }
-
-      // Reset the sum of squares and sample counter
-      Leq_sum_sqr = 0;
-      Leq_samples = 0;
-    }
-  }
-}
-
-void battery_checker_task(void* parameter) {
-  TickType_t xLastWakeTime = xTaskGetTickCount();
-  float shuntvoltage = 0;
-  float busvoltage = 0;
-  float current_mA = 0;
-  float loadvoltage = 0;
-
-  while (true) {
-    shuntvoltage = ina219.getShuntVoltage_mV();
-    busvoltage = ina219.getBusVoltage_V();
-    loadvoltage = busvoltage + (shuntvoltage / 1000);
-    current_mA = ina219.getCurrent_mA();
-
-    if (current_mA > MIN_CHARGING_CURRENT) {
-      batteryState = CHARGING;
-      // Turn on charging indicator LED
-      digitalWrite(CHARGER_LED_PIN, LOW);
-      Serial.println("[INFO] [POWER]: The battery is charging.");
-    } else if (loadvoltage > MIN_CHARGED_VOLTAGE) {
-      batteryState = ENOUGH_BATTERY;
-      // Turn off charging indicator LED
-      digitalWrite(CHARGER_LED_PIN, HIGH);
-      Serial.println("[INFO] [POWER]: The battery is charged.");
-    } else{
-      batteryState = LOW_BATTERY;
-      // Turn on charging indicator LED
-      digitalWrite(CHARGER_LED_PIN, LOW);
-      Serial.println("[WARNING] [POWER]: The battery voltage is low.");
-    }
-
-    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(BATTERY_CHECK_PERIOD));
-  }
-}
+#define LOGGER_TASK_PRI 1
+#define LOGGER_TASK_STACK 4096
 
 
-//
-// Setup and main loop 
-//
-// Note: Use doubles, not floats, here unless you want to pin
-//       the task to whichever core it happens to run on at the moment
-// 
 void setup() {
-  setCpuFrequencyMhz(240);  
-
-  // ledc (PWM) frequency and resolution setup
-  ledcAttach(RED_LED_PIN, LEDC_FREQ, LEDC_RESOLUTION);
-  ledcAttach(GREEN_LED_PIN, LEDC_FREQ, LEDC_RESOLUTION);
-  ledcAttach(BLUE_LED_PIN, LEDC_FREQ, LEDC_RESOLUTION);
+  setCpuFrequencyMhz(240);
 
   // Init serial for logging
   Serial.begin(115200);
 
-  WiFi.begin(ssid, password);
-  vTaskDelay(pdMS_TO_TICKS(5000));
-
   // Create FreeRTOS queue
   samples_queue = xQueueCreate(8, sizeof(float));
 
-  #ifdef USE_BATTERY
-  Wire.begin(INA_SDA, INA_SCL); // SDA, SCL
-
-  // Initialize the INA219.
-  // By default the initialization will use the largest range (32V, 2A).  However
-  // you can call a setCalibration function to change this range (see comments).
-  if (!ina219.begin()) {
-    Serial.println("[ERROR] [POWER]: Failed to find INA219 chip.");
-  } else {
-    pinMode(CHARGER_LED_PIN, OUTPUT);
-    // Turn off charging indicator LED
-    digitalWrite(CHARGER_LED_PIN, HIGH);
-
-    xTaskCreatePinnedToCore(battery_checker_task, "Battery Checker", BAT_TASK_STACK, NULL, BAT_TASK_PRI, NULL, 1);
-  }
-  #endif
+  initColorPins();
 
   // Create the mic reader task and pin it to the first core (ID=0)
   xTaskCreatePinnedToCore(mic_i2s_reader_task, "Mic I2S Reader", I2S_TASK_STACK, NULL, I2S_TASK_PRI, NULL, 0);
@@ -359,9 +93,18 @@ void setup() {
   // Create the Leq calculator task and pin it to the second core (ID=1)
   xTaskCreatePinnedToCore(leq_calculator_task, "Leq Calculator", LEQ_TASK_STACK, NULL, LEQ_TASK_PRI, NULL, 1);
 
+  #ifdef USE_BATTERY
+  xTaskCreatePinnedToCore(battery_checker_task, "Battery Checker", BAT_TASK_STACK, NULL, BAT_TASK_PRI, NULL, 1);
+  #endif
+
   #ifdef USE_LOGGING
   // Create the WiFi connection task and pin it to the second core (ID=1)
   xTaskCreatePinnedToCore(wifi_checker_task, "WiFi Checker", WIFI_TASK_STACK, NULL, WIFI_TASK_PRI, NULL, 1);
+  
+  // Initialize the logging queue
+  logging_queue = xQueueCreate(10, sizeof(LogData));
+  // Create the logger task and pin it to the second core (ID=1)
+  xTaskCreatePinnedToCore(logger_task, "Logger", LOGGER_TASK_STACK, NULL, LOGGER_TASK_STACK, NULL, 1);
   #endif
 
   vTaskDelay(pdMS_TO_TICKS(1000)); // Safety
